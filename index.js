33503d084f816ebf3a97fc8c5a1cca43fac3a479
GRACE-GG <gracewema607@gmail.com>
2024-02-21 18:15:54 +0300

    js commit

---
+++ index.js
@@ -0,0 +1,23 @@
+//console.log(`HELLO`);
+//console.log("I like pizza!");
+//window.alert(`This is an alert`);
+//window.alert(`I like pizza!`);
+
+document.getElementById("myH1").textContent = `Hello` ;
+document.getElementById("myP").textContent =`I like pizza!`;
+
+//This is a comment
+
+/*
+This
+is
+a
+comment
+*/
+
+//variable = a container that stores a value
+// behaves as if it were the value it contains.
+
+//1.declaration  let x;
+//2.assignment   x=100;
+                
