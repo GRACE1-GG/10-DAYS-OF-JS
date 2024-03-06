e662df089552992852fa393732932922a93244b4
GRACE-GG <gracewema607@gmail.com>
2024-03-06 12:49:39 +0300

    js commit

---
+++ index.js
@@ -0,0 +1,39 @@
+//console.log(`Hello`);
+//console.log(`I like pizza`);
+
+//window.alert(`This is an alert!`);
+//window.alert(`I like pizza!`);
+//alert window is used to change the text content of an HTML element
+
+//document.getElementById("myH1").textContent =`Hello`;
+//document.getElementById("myP").textContent = `I like pizza`;
+
+//we have created an alert
+
+/*this type of comment
+is used in multiline comments
+*/
+
+//VARIABLES-stores a value- 1)declaration letx; 2)assignment x=100;
+//ways
+//let gpa= 2.5;
+//let age = 25;
+//let price= 10.99;
+
+
+//console.log(typeof gpa);
+//console.log(`Your gpa is: ${gpa}`)
+//console.log(`You are ${age} years old`);
+//console.log(`The price is ${price}`);
+
+
+//let firstName = "Bro";
+//let favoriteFood = "pizza";
+//let email = "Bro@email.com";
+
+//console.log(typeof firstName);
+//console.log(`Your name is ${firstName}`);
+//console.log(`You like ${favoriteFood}`);
+//console.log(`Your email is ${email}`);
+
+let online 
